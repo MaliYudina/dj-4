18a357f945a3606598ba4e6c127ec0d6c4797d5b
Mali <maliyudina@gmail.com>
2019-06-26 22:49:48 +0300

    completed HW

--- site-form-works/car_admin/app/admin.py
+++ site-form-works/car_admin/app/admin.py
@@ -1,15 +1,21 @@
 from django.contrib import admin
 
-from .models import Car, Review
+from .models import *
 from .forms import ReviewAdminForm
 
 
 class CarAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('brand', 'model', 'get_reviews')
+    filter = ('brand', 'model')
+    search_fields = ('brand', 'model')
+    ordering = ('pk',)
 
 
 class ReviewAdmin(admin.ModelAdmin):
     form = ReviewAdminForm
+    list_display = ('car', 'title',)
+    filter = ('title', 'car',)
+    search_fields = ('car__model',)
 
 
 admin.site.register(Car, CarAdmin)
