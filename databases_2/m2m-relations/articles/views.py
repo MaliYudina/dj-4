24d62fa0614cb9a0328d5743e5b3cb7887e134de
Mali <maliyudina@gmail.com>
2019-07-05 00:15:56 +0300

    completed m2m

--- databases_2/m2m-relations/articles/views.py
+++ databases_2/m2m-relations/articles/views.py
@@ -1,9 +1,22 @@
-from django.views.generic import ListView
+from django.db.models import Prefetch
+from django.shortcuts import render
+from articles.models import *
 
-from articles.models import Article
 
+def articles_list(request):
+    template = 'articles/news.html'
+    context = {}
+    ordering = '-published_at'  # сортируем статьи по дате
 
-class ArticleListView(ListView):
-    template_name = 'articles/news.html'
-    model = Article
-    ordering = '-published_at'
+    # используйте этот параметр для упорядочивания результатов
+    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+    # The negative sign in front of "-pub_date" indicates descending order.
+    # Ascending order is implied. To order randomly, use "?"
+    articles = Article.objects.order_by(ordering).prefetch_related(
+        Prefetch('scopes', queryset=ArticleScope.objects.select_related(
+            'topic').order_by(
+            '-is_main'))).order_by('topic')
+
+    context['object_list'] = articles
+
+    return render(request, template, context)
