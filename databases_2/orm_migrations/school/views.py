85b648b03412e39fb772debf2691f10a99224b1b
Mali <maliyudina@gmail.com>
2019-07-04 15:17:06 +0300

    completed DB p.2 (orm_migrations)

--- databases_2/orm_migrations/school/views.py
+++ databases_2/orm_migrations/school/views.py
@@ -1,8 +1,14 @@
-from django.views.generic import ListView
+from django.shortcuts import render
 
 from .models import Student
 
 
-class StudentListView(ListView):
-    model = Student
+def students_list(request):
+    template = 'school/students_list.html'
+
+    context = {}
     ordering = 'group'
+    students = Student.objects.order_by(ordering).prefetch_related('teacher')
+    context['object_list'] = students
+
+    return render(request, template, context)
